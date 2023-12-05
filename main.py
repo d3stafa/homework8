bcfeb6c072f433bb5383737c6d579eb1fd3502a9
d3stafa <timagripich0407gmail.comtimagripich0407gmail.com>
2023-12-05 17:18:51 +0100

    main

--- main.py
+++ main.py
@@ -1,35 +1,26 @@
 from datetime import datetime, timedelta
 
+
 def get_birthdays_per_week(users):
-    # Отримуємо поточну дату
-    today = datetime.now().date()
+    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    today = datetime.today()
+    next_week = today + timedelta(days=7)
+    for i in range(7):
+        day = today + timedelta(days=i)
+        if day.weekday() == 6:
+            continue
+        print(f"{weekdays[i]}: ", end="")
+        for user in users:
+            if user['birthday'].weekday() == i:
+                print(f"{user['name']}, ", end="")
+        print()
+        if day == next_week:
+            break
+    users = [
+        {'name': 'Bill', 'birthday': datetime(2023, 1, 4)},
+        {'name': 'Jill', 'birthday': datetime(2023, 1, 6)},
+        {'name': 'Kim', 'birthday': datetime(2023, 1, 7)},
+        {'name': 'Jan', 'birthday': datetime(2023, 1, 8)}
+    ]
 
-    # Створюємо словник для збереження іменинників за кожний день тижня
-    birthdays_per_day = {i: [] for i in range(7)}
-
-    # Заповнюємо словник іменинниками
-    for user in users:
-        birthday = user['birthday'].date()
-        days_until_birthday = (birthday - today).days
-
-        if 0 <= days_until_birthday <= 7:
-            day_of_week = (today + timedelta(days_until_birthday)).weekday()
-            birthdays_per_day[day_of_week].append(user['name'])
-
-    # Виводимо результат
-    days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    for day, celebrants in birthdays_per_day.items():
-        day_name = days_of_week[day]
-        if celebrants:
-            print(f"{day_name}: {', '.join(celebrants)}")
-
-# Тестовий список користувачів
-users = [
-    {'name': 'Bill', 'birthday': datetime(2023, 1, 4)},
-    {'name': 'Jill', 'birthday': datetime(2023, 1, 6)},
-    {'name': 'Kim', 'birthday': datetime(2023, 1, 7)},
-    {'name': 'Jan', 'birthday': datetime(2023, 1, 8)}
-]
-
-# Викликаємо функцію для тестування
-get_birthdays_per_week(users)
+    get_birthdays_per_week(users)
